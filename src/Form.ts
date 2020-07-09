544f702f3f30b5d22fbb113fc254c16cfa2ed888
Leszekbugaj <48981350+Leszekbugaj@users.noreply.github.com>
2020-07-09 08:26:16 +0200

    Update Form.ts

--- src/Form.ts
+++ src/Form.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Field } from "./Field.js";
 import { FieldType } from "./FieldType.js";
 import { CheckboxField } from "./CheckboxField.js";
@@ -6,7 +5,7 @@ import { CheckboxField } from "./CheckboxField.js";
 export class Form {
     Fields: Field[] = [];
 
-    getValue() {
+    GetValue() {
 
         this.Fields.forEach(field => {
             const element: any = document.querySelector(`#${field.Name}`);
@@ -65,72 +64,4 @@ export class Form {
         return result;
     }
 
-=======
-import { Field } from "./Field.js";
-import { FieldType } from "./FieldType.js";
-import { CheckboxField } from "./CheckboxField.js";
-
-export class Form {
-    Fields: Field[] = [];
-
-    getValue() {
-
-        this.Fields.forEach(field => {
-            const element: any = document.querySelector(`#${field.Name}`);
-
-            switch(field.Type)
-            {
-                case FieldType.CheckboxField:
-                    console.log(`${element.id} => ${element.checked}`);
-                    break;
-                case FieldType.DateField:
-                case FieldType.EmailField:
-                case FieldType.InputField:
-                case FieldType.TextAreaField:
-                    console.log(element.id + '=>' + element.value);
-                    break;
-                case FieldType.SelectField:
-                    console.log(element.id + '=>' + element.options.item(element.options.selectedIndex)?.innerText);
-                    break;
-                default:
-                    break;
-                
-            }
-        });
-
-        // const input: NodeListOf<HTMLInputElement> = document.querySelectorAll("input, TextArea");
-        // const select: NodeListOf<HTMLSelectElement> = document.querySelectorAll("select");
-        // const option: NodeListOf<HTMLElement> = document.querySelectorAll("checkbox");
-
-        // input.forEach(function(element: HTMLInputElement){
-        //    console.log(element.id + '=>' + element.value);
-        // });
-
-        // select.forEach(function(element: HTMLSelectElement){
-        //     console.log(element.id + '=>' + element.options.item(element.options.selectedIndex)?.innerText);
-        // });
-
-        // option.forEach(function(element: HTMLElement){
-        //     console.log(element.id + '=>' + (element as HTMLInputElement).checked);
-        // });
-            
-
-
-        //     values?.push((node as HTMLInputElement).id);
-        //    if(node.nodeName === "SELECT") values?.push((node as HTMLSelectElement).options.item((node as HTMLSelectElement).options.selectedIndex)?.innerText);
-        //    else values?.push((node as HTMLInputElement).value);
-        // });
-        // console.table(values.filter(x => !!x));
-    }
-
-    render(): string {
-        let result: string = ``;
-        this.Fields.forEach(field => {
-            result += field.render();
-        });
-        result += `<br /><button onclick="Form.getValue()">Click</button><br />`
-        return result;
-    }
-
->>>>>>> 81c1e8b7dad46ca580f2f566df871b9916e660a6
 }
